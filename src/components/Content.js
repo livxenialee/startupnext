e2435d17bacaaa116ba2d5f4a33b73d7d30bc518
livxenialee <livxenialee@gmail.com>
2025-01-09 11:09:23 +0100

    Startup

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,14 +3,45 @@
 import DesktopView from "./DesktopView";
 import MobileView from "./MobileView";
 import React from 'react'
-import { useEffect ,useState } from "react";
+//import { useEffect ,useState } from "react";
 import windowWidth from "@/hooks/windowWidth";
 
-export default function content() {
+const Content = ({item}) => {
+
+  //const [dimension, setDimension] = useState(0);
 
   const width = windowWidth();
-  
-  const breakpoint = 620;
 
-  return width < breakpoint ? console.log("mobile") : console.log("desktop");
-} 
+  // useEffect( () => {
+
+  // setDimension(window.innerWidth);
+
+
+  //   const handleResize = () => {
+
+  //     setDimension(window.innerWidth);
+
+  //     console.log(window.innerWidth);
+  //   }
+
+
+  //   window.addEventListener("resize", handleResize)
+
+  //   return () => {
+
+  //     window.removeEventListener("resize", handleResize);
+
+  //   }
+
+  // }, [] );
+
+
+  const breakPoint = 620;
+
+  return width < breakPoint ? <MobileView item={item} /> : <DesktopView item={item} />;
+
+};
+
+export default Content;
+
+
